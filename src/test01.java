fbd02b18d44a89d9ecc0421835e29808894acc8a
luoshiwen <71312670+luoshiwen@users.noreply.github.com>
2020-12-30 15:25:24 +0800

    Update test01.java

--- src/test01.java
+++ src/test01.java
@@ -1,7 +1,7 @@
 public class test01 {
     public static void main(String[] args) {
         System.out.println("AAAA");
-        System.out.println("BBBB");
+        System.out.println("BB");
     }
 
 }
