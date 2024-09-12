d7cf65ab75a8bded46a13f7e9795e04a84895841
luoshiwen <18307581258@163.com>
2024-09-13 00:37:29 +0800

    测试

--- src/test01.java
+++ src/test01.java
@@ -2,6 +2,7 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("AAAA");
         System.out.println("BBBB");
+        System.out.println("CCCC");
     }
 
 }
